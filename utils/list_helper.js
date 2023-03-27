22860ca3c387ee5559966d731bdb71717f9ea809
plalgiush <ilyasushko@yandex.ru>
2023-03-27 17:26:40 +0300

    resolved the task 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,19 @@
-const dummy = (blogs) => {
-  return 1
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, blog) => total + blog.likes, 0)
+}
+
+const empty = (array) => {
+  return array.length === 0
+    ? 0
+    : null
+}
+
+const maxLikes = (blogs) => {
+  return blogs.reduce((acc, curr) => acc.likes > curr.likes ? acc : curr)
 }
 
 module.exports = {
-  dummy
+  totalLikes,
+  empty,
+  maxLikes
 }
